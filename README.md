a94883b93924e9dacfaa5372562f59c1d7fe8960
Galen.Yuan <dexin.yuan@innocellence.com>
2016-02-28 01:22:40 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# vue-datepicker
-A date picker for vuejs, it's still in developing.
+# Vue-DatePicker
+##Intro
+A date picker for [Vuejs](https://github.com/vuejs/vue/ "Vue"), it's still in developing.
+##Progress
+- Show Year & Month & Date <font color=green>Done</font>
+- Prev Month & Next Month <font color=green>Done</font>
+- Date Checker <font color=green>Done</font>
+- Save result to model <font color=red>In Progress</font>
+- Month Panel & Year Panel <font color=red>In Progress</font>
+- Build with Webpack <font color=red>In Progress</font>
+- Make it as a Plug-in of Vue <font color=red>In Progress</font>
+- Rewirte style with Bootstrap <font color=orange>Maybe</font>
+- Make it Responsive <font color=orange>Maybe</font>
+- Time Picker <font color=orange>Maybe</font>
---
+++ vue-datepicker.js
@@ -0,0 +1,87 @@
+var app = new Vue({
+	el: "#wrap",
+	data:{
+		_show:false,
+		current:{
+			year:0,
+			month:0,
+			date:0,
+			weekDay:0
+		},
+		dateList:[],
+		beforeDateList:[],
+		afterDateList:[]
+	},
+	created:function(){
+		this.render();
+	},
+	methods:{
+		render:function(model){
+			if(model){
+				var currentDate = new Date(model);
+			}else{
+				var currentDate = new Date();
+			}
+			var currentYear = currentDate.getFullYear();
+			var currentMonth = currentDate.getMonth();
+			var currentWeekDay = currentDate.getDay();
+			var currentFirstDay = new Date(currentYear,currentMonth,1);
+			var currentLastDay = new Date(currentYear,currentMonth+1,0).getDate();
+			this.current.year = currentYear;
+			this.current.month = currentMonth + 1;
+			this.current.date = currentDate.getDate();
+			this.current.weekDay = currentDate.getDay();
+			this.dateList = [];
+			for(var i=1;i<=currentLastDay;i++){
+				var a = {
+					date:i,
+					checked:false
+				}
+				if(currentDate.getDate()===i) a.checked = true;
+				this.dateList.push(a);
+			}
+			this.beforeDateList = [];
+			var lastMonthEnd = new Date(currentYear,currentMonth,0).getDate();
+			for(var i=lastMonthEnd,a=lastMonthEnd+1-currentFirstDay.getDay();a<=i;i--){
+				this.beforeDateList.push(i);
+			}
+			this.beforeDateList.reverse();
+			this.afterDateList = [];
+			var currentLastWeekDay = new Date(currentYear,currentMonth+1,0).getDay();
+			for(var i=1,a=6-currentLastWeekDay;i<=a;i++){
+				this.afterDateList.push(i);
+			}
+		},
+		checkDate:function(item){
+			for(var i=0,a=this.dateList.length;i<a;i++){
+				if(this.dateList[i].checked){
+					this.dateList[i].checked = false;
+				}
+			}
+			item.checked = true;
+			this.current.date = item.date;
+			var result = new Date(this.current.year,this.current.month-1,this.current.date).toLocaleDateString();
+			console.log(result)
+		},
+		prevMonth:function(currentMonth){
+			this.current.month = currentMonth - 1;
+			if(currentMonth===1){
+				this.current.month = 12;
+				this.current.year--;
+			}
+			this.current.date = 1;
+			var prevDate = new Date(this.current.year,this.current.month-1,this.current.date)
+			this.render(prevDate);
+		},
+		nextMonth:function(currentMonth){
+			this.current.month = currentMonth + 1;
+			if(currentMonth===12){
+				this.current.month = 1;
+				this.current.year++;
+			}
+			this.current.date = 1;
+			var nextDate = new Date(this.current.year,this.current.month-1,this.current.date)
+			this.render(nextDate);
+		}
+	}
+})
